e95f677e9c3649d1219169f93df76d9135d1f837
fernandorenan <nuncavouteabandonar@gmail.com>
2023-11-02 16:47:03 -0300

    Projeto

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Peeper
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    #Config Cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:8080' # Substitua pelo endereço do seu aplicativo Vue.js
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
+  end
+end
